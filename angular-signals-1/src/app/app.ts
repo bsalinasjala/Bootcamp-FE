41f92aedcb0c609203553cff62b80e9a743d561e
bsalinasjala <bruno.salinas@jala.university>
2026-05-27 16:43:23 -0400

    solution to window resize and button to menu

--- angular-signals-1/src/app/app.ts
+++ angular-signals-1/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,17 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-signals-1');
-  protected readonly menuOpen = signal(true);
+  protected readonly title = signal<string>('angular-signals-1');
+  protected readonly menuOpen = signal<boolean>(true);
 
   protected actionMenu(): void {
     this.menuOpen.update((value) => !value);
+  }
+
+  @HostListener('window:resize')
+  protected closeMenuOnSmallScreen(): void {
+    if (window.innerWidth <= 768) {
+      this.menuOpen.set(false);
+    }
   }
 }
