6408c3bad29a5d7fe9dc8526884a8472c8e28fc8
bsalinasjala <bruno.salinas@jala.university>
2026-06-08 12:09:36 -0400

    feature: add module 13(Two-Way Binding)

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RecipeModel } from '../models';
 import { MOCK_RECIPES } from '../mock-recipes';
 import { RecipeDetail } from '../recipe-detail/recipe-detail';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [RecipeDetail],
+  imports: [RecipeDetail, FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
@@ -17,4 +18,9 @@ export class RecipeList {
   protected selectRecipe(recipe: RecipeModel): void {
     this.selectedRecipe.set(recipe);
   }
+
+  protected readonly searchTerm = signal<string>('');
+  protected readonly filteredRecipes = computed(() =>
+    this.recipes.filter(recipe =>
+      recipe.name.toLowerCase().includes(this.searchTerm().toLowerCase())));
 }
