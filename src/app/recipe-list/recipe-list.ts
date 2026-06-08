b3c303cfec71b99863fae7b93e52402f6cf22247
bsalinasjala <bruno.salinas@jala.university>
2026-06-08 14:19:14 -0400

    feature: add module 14(Services & Dependency Injection (DI))

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,8 +1,8 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, signal, inject } from '@angular/core';
 import { RecipeModel } from '../models';
-import { MOCK_RECIPES } from '../mock-recipes';
 import { RecipeDetail } from '../recipe-detail/recipe-detail';
 import { FormsModule } from '@angular/forms';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,8 +10,9 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
+
 export class RecipeList {
-  protected readonly recipes = MOCK_RECIPES;
+  protected readonly recipes = inject(Recipe).getRecipes();
 
   protected readonly selectedRecipe = signal<RecipeModel>(this.recipes[0]);
 
---
+++ src/app/recipe.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { MOCK_RECIPES } from './mock-recipes';
+import { RecipeModel } from './models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Recipe {
+  getRecipes(): RecipeModel[] {
+    return MOCK_RECIPES;
+  }
+}
